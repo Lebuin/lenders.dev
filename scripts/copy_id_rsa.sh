a550e4246ceab0562a9ff4d5300e847da9840f8b
Seppe Lenders <seppe.lenders@gmail.com>
2021-01-29 17:03:18 +0100

    update copy_id_rsa script

--- scripts/copy_id_rsa.sh
+++ scripts/copy_id_rsa.sh
@@ -2,5 +2,6 @@
 
 # This script should be run on your local machine, not on the server
 
+scp ~/.ssh/id_rsa.pub ubuntu@lenders.dev:.ssh/authorized_keys
 scp ~/.ssh/id_rsa_seppe.dev ubuntu@lenders.dev:.ssh/id_rsa
 scp ~/.ssh/id_rsa_seppe.dev.pub ubuntu@lenders.dev:.ssh/id_rsa.pub
